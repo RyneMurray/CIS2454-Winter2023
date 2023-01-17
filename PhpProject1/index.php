ba6d01eff70ae349d997d7dd69dea1d596e9f7d4
RyneMurray <murrayryne@gmail.com>
2023-01-17 10:50:59 -0500

    Ryne Murray Lab Zero

---
+++ PhpProject1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<h2> My partner's name is Mohamed Alkaysey </h2></br>";
+        echo "</br><h3> He has taken the following classes before this one: "
+        . "CIS 1200, 1300, 1420, 1440, 1500, 2151, 2353, 2111, 2252 </h3>";
+        echo "</br><h3> His hobbies include "
+        . "playing video games, reading books every now and then, watching movies,"
+                . " and spending way too much time on tiktok </h3>";
+        echo "</br><h3>He is hoping to transfer his credits to OU in hopes of "
+        . "continuing his degree in CIS Software Engineering </h3>";
+        ?>
+    </body>
+</html>
+
+
